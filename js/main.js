820d48f55d4bcd701bee4e570fe7ce8c702d10b8
Allen <allenonrails@zohomail.eu>
2023-12-09 02:05:34 +0300

    products popup

--- js/main.js
+++ js/main.js
@@ -59,6 +59,165 @@ class Slider {
   }
 }
 
+class CoverFlow {
+  constructor() {
+      this.OVERLAP = 0.25;
+      this.ROTATION = 45;
+      this.DELAY = 250;
+      this.SWIPE = 75;
+      this.content = [...document.querySelectorAll('div.content')];
+      this.index = Math.floor(this.content.length / 2);
+
+      this.init();
+  }
+
+  action_flip(show) {
+      let current = this.content[this.index];
+      let shown = current.classList.contains('selected');
+      let change = false;
+
+      if (show && !shown) {
+          change = true;
+          current.classList.add('selected');
+          current.style.transform = `rotateY(180deg) rotateZ(90deg) scale(${window.innerWidth <= 420 ? 1 : 1.2})`;
+      } else if (shown && !show) {
+          change = true;
+          current.style.transform = 'rotateY(0deg)';
+          setTimeout(() => { current.classList.remove('selected') }, this.DELAY);
+      }
+
+      return change;
+  }
+
+  action_prev() {
+      if (this.index) {
+          this.index--;
+          this.action_flow();
+      }
+  }
+
+  action_next() {
+      if (this.content.length > (this.index + 1)) {
+          this.index++;
+          this.action_flow();
+      }
+  }
+
+  action_goto(i) {
+      if (this.index !== i) {
+          this.index = i;
+          this.action_flow();
+      }
+  }
+
+  action_flow() {
+      this.content.forEach((c, i) => {
+          let transform = '';
+          let zindex = '';
+          let offset = c.clientWidth * this.OVERLAP;
+
+          if (i < this.index) {
+              transform = `translateX(-${offset * (this.index - i)}%) rotateY(${this.ROTATION}deg)`;
+              zindex = i;
+          } else if (i === this.index) {
+              transform = 'rotateY(0deg) translateZ(140px)';
+              zindex = this.content.length;
+          } else {
+              transform = `translateX(${offset * (i - this.index)}%) rotateY(-${this.ROTATION}deg)`;
+              zindex = this.content.length - i;
+          }
+
+          c.style.transform = transform;
+          c.style.zIndex = zindex;
+          c.classList.remove('current');
+      });
+
+      setTimeout(() => { this.content[this.index].classList.add('current') }, this.DELAY);
+  }
+
+  state(event, context) {
+    const eventHandlers = {
+        'left': () => this.action_flip(false) || this.action_prev(),
+        'right': () => this.action_flip(false) || this.action_next(),
+        'select': () => context === this.index ? this.action_flip(true) : (this.action_flip(false) || this.action_goto(context)),
+        'submit': () => this.action_flip(true),
+        'dismiss': () => this.action_flip(false)
+    };
+
+    const handler = eventHandlers[event];
+    if (handler) {
+        handler();
+    }
+  }
+
+
+  events() {
+      document.addEventListener('keydown', event => {
+          const EVENTS = {
+              ArrowLeft: 'left',
+              ArrowRight: 'right',
+              Enter: 'submit',
+              Backspace: 'dismiss',
+              Escape: 'dismiss'
+          };
+
+          this.state(EVENTS[event.key]);
+      });
+
+      document.addEventListener('mouseup', event => {
+          if (event.target.classList.contains('coverflow')) this.state('dismiss');
+      });
+
+      let touched = 0;
+
+      document.addEventListener('touchstart', event => {
+          touched = event.changedTouches[0].screenX;
+      });
+
+      document.addEventListener('touchend', event => {
+          let moved = touched - event.changedTouches[0].screenX;
+          if (moved < 0 && Math.abs(moved) > this.SWIPE) this.state('left');
+          if (moved > 0 && Math.abs(moved) > this.SWIPE) this.state('right');
+      });
+
+      addEventListener('resize', () => this.action_flow());
+  }
+
+  init() {
+      this.content.forEach((c, i) => {
+          c.onclick = () => this.state('select', i);
+          c.style.zIndex = this.index === i ? 1 : 0;
+      });
+
+      setTimeout(() => this.action_flow(), this.DELAY);
+      this.events();
+  }
+}
+
+new CoverFlow();
+
+function modal(){
+  let modal = document.querySelector('.modal');
+
+  document.querySelectorAll('.open-modal').forEach(btn => {
+    btn.addEventListener('click', function(e){
+      e.preventDefault()
+
+      modal.classList.add('modal-active')
+      modal.style.animation = 'sliderInner .9s ease-in-out forwards'    
+    })
+  });
+
+  document.querySelector('.close').addEventListener('click', async function(e){
+    e.preventDefault()
+
+    
+    modal.style.animation = 'sliderOut .9s ease-in-out forwards'  
+    await sleep(1300)
+    modal.classList.remove('modal-active')
+  })
+}
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -122,185 +281,8 @@ document.addEventListener('DOMContentLoaded', function(){
     })
     
   })
-  // --- Released as KarmaWare by Pete Rai - http://pete.rai.org.uk/
+  const coverFlow = new CoverFlow();
 
-// --- various fiddle bit - have a play
-
-const OVERLAP = 0.25; // of width
-const ROTATION = 45; // degrees
-const DELAY = 250; // milliseconds - for various animations
-const SCALE = 1.7; // for when card flips to back
-const SWIPE = 75; // pixels - min swipe length
-
-// --- globals
-
-var content = [ ...document.querySelectorAll('div.content')];
-var index = Math.floor(content.length / 2);
-
-// --- flips an item over
-
-function action_flip(show) {
-    let current = content[index];
-    let shown = current.classList.contains('selected');
-    let change = false;
-
-    if (show && !shown) {
-        change = true;
-        current.classList.add('selected');
-
-        if (window.innerWidth <= 420) {
-          current.style.transform = `rotateY(180deg) rotateZ(90deg) scale(1)`;
-        }else {
-          current.style.transform = `rotateY(180deg) rotateZ(90deg) scale(1.2)`;
-        }
-        
-    }
-    else if (shown && !show)
-    {
-        change = true;
-        current.style.transform = 'rotateY(0deg)';
-        setTimeout (() => { current.classList.remove('selected') }, DELAY);
-    }
-
-    return change;
-}
-
-// --- move to previous item
-
-function action_prev() {
-    if (index) {
-        index--;
-        action_flow();
-    }
-}
-
-// --- move to next item
-
-function action_next() {
-    if (content.length > (index + 1)) {
-        index++;
-        action_flow();
-    }
-}
-
-// --- jump to specified item
-
-function action_goto(i) {
-    if (index !== i) {
-        index = i;
-        action_flow();
-    }
-}
-
-// --- reflow the items
-
-function action_flow() {
-    content.forEach((c, i) =>  {
-        let transform = '';
-        let zindex = '';
-        let offset = c.clientWidth * OVERLAP;
-
-        if (i < index) {
-            transform = `translateX(-${ offset * (index - i) }%) rotateY(${ ROTATION }deg)`;
-            zindex = i;
-        }
-
-        else if (i === index) {
-            transform = 'rotateY(0deg) translateZ(140px)';
-            zindex = content.length;
-        }
-
-        else /* if (i > index) */ {
-            transform = `translateX(${ offset * (i - index) }%) rotateY(-${ ROTATION }deg)`;
-            zindex = content.length - i;
-        }
-
-        c.style.transform = transform;
-        c.style.zIndex = zindex;
-        c.classList.remove('current');
-    });
-
-    setTimeout (() => { content[index].classList.add ('current') }, DELAY);
-}
-
-// --- state management
-
-function state(event, context) {
-
-    if (event === 'left') {
-        action_flip(false) || action_prev();
-    }
-
-    else if (event === 'right') {
-        action_flip(false) || action_next();
-    }
-
-    else if (event === 'select') {
-        context === index ?  action_flip(true) : (action_flip(false) || action_goto(context));
-    }
-
-    else if (event === 'submit') {
-        action_flip(true);
-    }
-
-    else if (event === 'dismiss') {
-        action_flip(false);
-    }
-
-    else {
-        // do nothing here
-    }
-}
-
-// --- event management
-
-function events() {
-
-    document.addEventListener('keydown', event => {
-        const EVENTS = {
-            ArrowLeft: 'left',
-            ArrowRight: 'right',
-            Enter: 'submit',
-            Backspace: 'dismiss',
-            Escape: 'dismiss'
-        };
-
-        state(EVENTS[event.key]);
-    });
-
-    document.addEventListener('mouseup', event => {
-        if (event.target.classList.contains('coverflow')) state('dismiss');
-    });
-
-    let touched = 0
-
-    document.addEventListener('touchstart', event => {
-        touched = event.changedTouches[0].screenX;
-    });
-
-    document.addEventListener('touchend', event => {
-        let moved = touched - event.changedTouches[0].screenX;
-        if (moved < 0 && Math.abs(moved) > SWIPE) state('left');
-        if (moved > 0 && Math.abs(moved) > SWIPE) state('right');
-    });
-  
-    addEventListener('resize', (event) => { action_flow() });
-}
-
-// --- initialisation 
-
-function init() {
-
-    content.forEach((c, i) =>  {
-        c.onclick = () => { state('select', i) };
-        c.style.zIndex = index === i ? 1 : 0;
-    });
-
-    setTimeout (() => { action_flow() }, DELAY);
-    events();
-}
-
-init();
-
+  modal()
 })
 
